1a10509bff7ceef0ca3392a9df5e4b7d0842ba65
gulsennur <gulsennur1995@gmail.com>
2019-11-16 00:35:53 +0300

    - Timer eklendi. -MessageBox.Show Overloadları incelendi.

--- Vektorel.Windows.KacanButonApp/frmOyun.cs
+++ Vektorel.Windows.KacanButonApp/frmOyun.cs
@@ -12,7 +12,8 @@ namespace Vektorel.Windows.KacanButonApp
 {
     public partial class frmOyun : Form
     {
-        int count=0;
+        int count = 0;
+        int sure = 5;
         public frmOyun()
         {
             InitializeComponent();
@@ -26,16 +27,45 @@ namespace Vektorel.Windows.KacanButonApp
 
         private void Button1_MouseMove(object sender, MouseEventArgs e)
         {
+            Random rnd = new Random();
+            this.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
             count++;
             if(count%10==0)
             {
-                this.Size = new Size(this.Width + 10, this.Height + 10);
+                this.Size = new Size(this.Width + 100, this.Height + 100);
             }
             btnKac.Text = count.ToString();
 
-            Random rnd = new Random();
-              
             btnKac.Location = new Point(rnd.Next (this.ClientSize.Width-btnKac.Width), rnd.Next(this.ClientSize.Height-btnKac.Height));
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            sure--;
+            this.Text = sure.ToString();
+            if (sure==0)
+            {
+                tmrSure.Stop();
+                DialogResult cvp = MessageBox.Show($"Süreniz doldu , puanınız:{ count}\n Yeniden oynamak istiyor musunuz?", "Oyun bitti",MessageBoxButtons.YesNo,MessageBoxIcon.Question );
+                if (cvp == DialogResult.Yes)
+                {
+                    sure = 5;
+                    count = 0;
+                    btnKac.Text = count.ToString();
+                    this.Size = new Size(300, 300);
+                    tmrSure.Start();
+                }
+                else
+                {
+                    btnKac.Text = ":D";
+                    btnKac.Enabled = false;
+                }
+            }
+        }
+
+        private void FrmOyun_Load(object sender, EventArgs e)
+        {
+            tmrSure.Start();
         }
     }
 }
